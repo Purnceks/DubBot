f218929c98d64c0b4f6540f01474fcbdc34a951a
Ben Wildeman <wildeman.benjamin@gmail.com>
2015-11-22 01:14:48 +0000

    just finished the motd command, added staff/ranks arrays to bot, and added in check to see if params are greater than 0 (oops, forgot to do that previously.

--- commands.js
+++ commands.js
@@ -39,7 +39,7 @@ module.exports = {
 	//function bot responses
 	define: function (data) {
 		var user = data.user.username;
-		if (typeof (data.params) != 'undefined') {
+		if (typeof (data.params) != 'undefined' && data.params.length > 0) {
 			if (data.params.length = 1) {
 				term = data.params[0];
 				request('http://api.urbandictionary.com/v0/define?term=' + term, function (error, response, body) {
@@ -96,7 +96,7 @@ module.exports = {
 	},
 	sendlove: function (data) {
 		var user = data.user.username;
-		if (typeof (data.params) != 'undefined') {
+		if (typeof (data.params) != 'undefined' && data.params.length > 0) {
 			if (data.params.length = 1) {
 				if (data.params[0].substr(0, 1) === '@') {
 					recipient = data.params[0];
@@ -120,7 +120,7 @@ module.exports = {
 			];
 		var taco = tacos[Math.floor(Math.random() * cookies.length)];
 		var user = data.user.username;
-		if (typeof (data.params) != 'undefined') {
+		if (typeof (data.params) != 'undefined' && data.params.length > 0) {
 			if (data.params.length = 1) {
 				if (data.params[0].substr(0, 1) === '@') {
 					recipient = data.params[0];
@@ -160,7 +160,7 @@ module.exports = {
 			];
 		var cookie = cookies[Math.floor(Math.random() * cookies.length)];
 		var user = data.user.username;
-		if (typeof (data.params) != 'undefined') {
+		if (typeof (data.params) != 'undefined' && data.params.length > 0) {
 			if (data.params.length = 1) {
 				if (data.params[0].substr(0, 1) === '@') {
 					recipient = data.params[0];
@@ -270,6 +270,73 @@ module.exports = {
 		var pizza = pizzas[Math.floor(Math.random() * pizzas.length)];
 		bot.sendChat(bot.identifier + pizza);
 	},
+	motd: function (data) {
+		var user = data.user.username;
+		var rank = data.user.role;
+		//if the user has name in the bot.staff array, or their role is one from bot.rank
+		if (bot.staff.indexOf(user) > -1 || bot.ranks.indexOf(rank) > -1) {
+			//checks to make sure there's params set
+			if (typeof (data.params) != 'undefined' && data.params.length > 0) {
+				//makes sure that there's more than one param (as motd can have two params, as well as the motd)
+				if (data.params.length > 1) {
+					firstParam = data.params[0];
+					//checks to see if the first param is a number, and doesn't return NaN
+					if (isNaN(parseInt(firstParam))) {
+						//checks to see if the first param is equal to set, if it is, remove it from the param list with slice, then join
+						// the rest to set the motd
+						if (firstParam === "set") {
+							var motd = data.params.slice(1).join(" ");
+							bot.motd = motd;
+							bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
+							//if it doesn't join the params together to set the motd
+						} else {
+							bot.motd = data.params.join(" ");
+							bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
+						}
+						//if the first param is a number
+					} else {
+						secondParam = data.params[1];
+						//checks to see if the second param is equal to set, it is, remove it and the interval from the param list with slice, then join
+						//then join together to set the motd
+						if (secondParam === "set") {
+							var motd = data.params.slice(2).join(" ");
+							bot.motdInterval = parseInt(firstParam);
+							bot.motd = motd;
+							bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
+							//if it doesn't, just remove the interval from the params, then join them together to set the motd
+						} else {
+							var motd = data.params.slice(1).join(" ");
+							botd.motdInterval = parseInt(firstParam);
+							bot.motd = motd;
+							bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd + " with interval of: " + bot.motdInterval);
+						}
+					}
+					//if the params is one
+				} else {
+					//checks to see if the only param is set
+					if (data.params[0] === "set") {
+						bot.sendChat(bot.identifier + "to set MOTD do: !motd [interval] set [motd message]");
+						//checks to see if the only param is a number
+					} else if (!isNaN(parseInt(data.params[0]))) {
+						bot.sendChat(bot.identifier + "to set MOTD do: !motd [interval] set [motd message]");
+					} else {
+						//single word motd (for that odd occasion when we might have just one word. who knows)
+						bot.motd = data.params[0];
+						bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
+					}
+				}
+				//if the command is on its lonesome
+			} else {
+				//if not set previously, say how to set
+				if (bot.motd === "") {
+					bot.sendChat(bot.identifier + "Motd not set. do '!motd [interval] set [motd message]' to set motd");
+					//if set previously, say what the motd is currently
+				} else {
+					bot.sendChat(bot.identifier + bot.motd);
+				}
+			}
+		}
+	},
 	//TODO/complete:
 	/*
 	die: function (data) {
@@ -290,34 +357,6 @@ module.exports = {
 			bot.sendChat("/me " + languser + " Englisch sprechen bitte");
 		} else {
 			bot.sendChat("/me " + languser + " Speak english please");
-		}
-	},
-	motd: function (data) {
-		var user = data.user.username;
-		if (typeof (data.params) != 'undefined') {
-			if (data.params.length > 1) {
-				if (data.params[0] === "set") {
-					var motd = data.params.slice(1).join(" ");
-					bot.motd = motd;
-					bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
-				} else {
-					bot.motd = data.params.join(" ");
-					bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
-				}
-			} else {
-				if (data.params[0] === "set") {
-					bot.sendChat(bot.identifier + "to set MOTD do: !motd set [motd message]");
-				} else {
-					bot.motd = data.params[0];
-					bot.sendChat(bot.identifier + "MOTD has been set to: " + bot.motd);
-				}
-			}
-		} else {
-			if (bot.motd === "") {
-				bot.sendChat(bot.identifier + "Motd not set. do '!motd [motd message]' to set motd")
-			} else {
-				bot.sendChat(bot.identifier + bot.motd);
-			}
 		}
 	},
 	*/
